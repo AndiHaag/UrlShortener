3b04568936b337ae4f072256572a29571a3691c3
Niclas Bammert <nb-201291@rwu.de>
2022-06-27 19:10:14 +0200

    Fix lala

--- src/components/Home/Input.vue
+++ src/components/Home/Input.vue
@@ -1,48 +1,85 @@
 <template>
     <h2>Paste the URL to be shortened</h2>
     <div class="input">
-        <input type="url" placeholder="Get your URL here">
-        <button>Shorten URL</button>
+        <input v-model="url" placeholder="Get your URL here">
+
+        <button v-on:click="shorten_url">Shorten URL</button>
     </div>
-    <div class="output">
-        <p>Textbox for URL</p>
+
+    <div class="output" v-if="posts !== undefined">
+        {{posts.result.full_short_link}} <br />
+        {{posts.result.full_short_link2}} <br />
+        {{posts.result.full_short_link3}}
+    </div>
+    <div class="output" v-if="posts == undefined">
+        <ul v-if="errors && errors.length">
+            <li v-for="error of errors" :key="error">
+                {{error.message}}
+            </li>
+        </ul>
     </div>
 </template>
 <script>
-export default{
-    name:'Input',
-}
+
+    import axios from 'axios';
+
+    export default {
+        data() {
+            return {
+                posts: undefined,
+                errors: [],
+                url: ""
+            }
+        },
+
+        methods: {
+            shorten_url: function (event) {
+                console.log("1")
+                this.apicall()
+            },
+
+            apicall: function () {
+                const urlstring = "https://api.shrtco.de/v2/shorten?url=" + this.url
+                axios.get(urlstring).then(response => {
+                    this.posts = response.data
+                }).catch(e => {
+                    this.errors.push(e)
+                })
+            }
+        }
+    }
+
 </script>
 
 <style>
-input {
-    width: 300px;
-    height: 40px;
-    padding-left: 20px;
-    margin-bottom: 30px;
-    margin-right: auto;
-    margin-left: auto;
-    border: 3px solid skyblue;
-}
+    input {
+        width: 300px;
+        height: 40px;
+        padding-left: 20px;
+        margin-bottom: 30px;
+        margin-right: auto;
+        margin-left: auto;
+        border: 3px solid skyblue;
+    }
 
-button {
-    width: 125px;
-    height: 47px;
-    border: 3px solid skyblue;
-    overflow: hidden;
-}
+    button {
+        width: 125px;
+        height: 47px;
+        border: 3px solid skyblue;
+        overflow: hidden;
+    }
 
-.input button:hover{
-    background: #ddd;
-    color: #333;
-}
+    .input button:hover {
+        background: #ddd;
+        color: #333;
+    }
 
-.output {
-    margin-top: 25px;
-    margin-left: 35%;
-    margin-right: 35%;
-    font-size: largerfdff;
-    font-weight: bolder;
-    border: 3px solid skyblue;
-}
+    .output {
+        margin-top: 25px;
+        margin-left: 35%;
+        margin-right: 35%;
+        font-size: largerfdff;
+        font-weight: bolder;
+        border: 3px solid skyblue;
+    }
 </style>
